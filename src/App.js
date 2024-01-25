f5c30939565fdd4d4d05503fccb5842698bffd72
Juan <justjuanking@gmail.com>
2024-01-25 11:24:52 -0500

    Incio de proyecto final

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+// import Greeting from './components/pure/greeting';
+// import GreetingF from './components/pure/greetingF';
+import TaskListComponent from './components/containers/task_list';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/*Componente propio Greeting.jsx*/}
+        {/* <Greeting name = "Juan"></Greeting> */}
+        {/* Componente ejemplo función */}
+        {/* <GreetingF name = "Juan"></GreetingF> */}
+        {/* Componente de listado de tareas  */}
+        <TaskListComponent></TaskListComponent>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
